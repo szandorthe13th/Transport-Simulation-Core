55f81d1766d37391f73d3bbadcc855d0dc497455
Szándor Czirják <131382798+szandorthe13th@users.noreply.github.com>
2025-07-25 03:05:53 +0200

    Update RouteType.java
    Ok now we're getting somewhere

--- src/main/java/org/mtr/core/data/RouteType.java
+++ src/main/java/org/mtr/core/data/RouteType.java
@@ -1,7 +1,7 @@
 package org.mtr.core.data;
 
 public enum RouteType {
-	NORMAL, LIGHT_RAIL, HIGH_SPEED;
+	NORMAL, LIGHT_RAIL, METRO, INTERCITY, HIGH_SPEED;
 
 	public RouteType next() {
 		return values()[(ordinal() + 1) % values().length];
